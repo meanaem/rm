e0fe1ace64bb84494f6c4d9b5bbbbb7e10fa30a3
VJ <aarintech@hotmail.com>
2015-03-24 21:07:16 -0500

    dependency libraries

--- components/openapi/src/main/java/com/aarintech/rm/openapi/domain/BookingControl.java
+++ components/openapi/src/main/java/com/aarintech/rm/openapi/domain/BookingControl.java
@@ -1,10 +1,13 @@
 package com.aarintech.rm.openapi.domain;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
  * Created by vireshkj on 3/22/15.
  */
+@Data
 public class BookingControl {
     private int capacity;
     private List<Cabin> cabins;
--- components/openapi/src/main/java/com/aarintech/rm/openapi/domain/Cabin.java
+++ components/openapi/src/main/java/com/aarintech/rm/openapi/domain/Cabin.java
@@ -1,8 +1,11 @@
 package com.aarintech.rm.openapi.domain;
 
+import lombok.Data;
+
 /**
  * Created by vireshkj on 3/22/15.
  */
+@Data
 public class Cabin {
     private int capacity;
     private int bookingLimit;
